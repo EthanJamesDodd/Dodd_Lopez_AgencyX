9e3e54c4b133cbf83e100e9620da7d63e46d15aa
Ethan <EthanJamesDodd@gmail.com>
2018-10-05 22:03:52 -0400

    Final Version

--- js/main.js
+++ js/main.js
@@ -1,16 +1,67 @@
 AOS.init();
-
+var pswpElement = document.querySelectorAll('.pswp')[0];
 let mainNav = document.querySelector('#js-menu'),
     navBarToggle = document.querySelector('#js-navbar-toggle'),
     nameBox = document.querySelector('#nameBox'),
     emailBox = document.querySelector('#emailBox'),
-    messageBox = document.querySelector('#messageBox');
+    messageBox = document.querySelector('#messageBox'),
+    pureVolume = document.querySelector('#workDisplay2'),
+    videoButton = document.querySelector('#vidBut'),
+    videoBox = document.querySelector('.lightBox'),
+    closeBox = document.querySelector('.closeButton'),
+    videoPlayer = document.querySelector('video');
 
+    var openPhotoSwipe = function() {
+        var pswpElement = document.querySelectorAll('.pswp')[0];
+    
+        // build items array
+        var items = [
+            {
+                src: 'images/pureVolumeDesktop.png',
+                w: 1600,
+                h: 3625
+            },
+            {
+                src: 'images/pureVolumeMobile.png',
+                w: 360,
+                h: 4259
+            },
+            {
+                src: 'images/articleDesktop.png',
+                w: 1600,
+                h: 5599
+            },
+            {
+                src: 'images/articleMobile.png',
+                w: 383,
+                h: 5309
+            },
+            
+        ];
+        
+        
+        var gallery = new PhotoSwipe( pswpElement, PhotoSwipeUI_Default, items, options);
+        gallery.init();
+    };
+    
 
 function hamMenu(){
     console.log('stuff');
     mainNav.classList.toggle('active');
 }
+
+function videoClose(){
+    videoBox.classList.remove('showLightBox');
+    videoPlayer.pause();
+    videoPlayer.currentTime = 0;
+}
+
+function videoPopUp(){
+    videoBox.classList.add('showLightBox');
+}
+
+
+
 
 // Cascading Scrolls
 ScrollReveal().reveal('.mainHeader');
@@ -21,4 +72,11 @@ ScrollReveal().reveal('.socialIcons', {delay: 1000});
 ScrollReveal().reveal('.teamDesc', {delay: 1300});
 
 
+
+
+
+
 navBarToggle.addEventListener('click', hamMenu);
+pureVolume.addEventListener('click', openPhotoSwipe);
+videoButton.addEventListener('click', videoPopUp);
+closeBox.addEventListener('click', videoClose);
